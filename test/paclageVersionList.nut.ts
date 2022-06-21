ad20193b30f60aec3172e1522dca32f96519a51b
Peter Hale <peter.hale@salesforce.com>
2022-06-21 08:00:27 -0600

    v0.0.4-test-01
    v0.0.4-test-02
    
    v0.0.4-test-03
    
    v0.0.4-test-04
    
    v0.0.4-test-05
    
    v0.0.4-test-06
    
    v0.0.4-test-07
    
    v0.0.4-test-08
    
    v0.0.4-test-09
    
    v0.0.4-test-10

---
+++ test/paclageVersionList.nut.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2022, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { TestSession } from '@salesforce/cli-plugins-testkit';
+import { ConfigAggregator, Org } from '@salesforce/core';
+import { expect } from 'chai';
+import { listPackages } from '../src/package';
+
+describe('package list', () => {
+  let session: TestSession;
+  let aliasOrUsername: string;
+  before(async () => {
+    session = await TestSession.create();
+    const config = await ConfigAggregator.create({});
+    aliasOrUsername = config.getPropertyValue('target-dev-hub');
+
+    if (!aliasOrUsername) throw Error('no default username set');
+  });
+
+  after(async () => {
+    await session?.clean();
+  });
+  it('should query Package2 table for all packages', async () => {
+    const org = await Org.create({ aliasOrUsername });
+    const results = await listPackages(org.getConnection());
+    expect(results).to.be.ok;
+  });
+});
