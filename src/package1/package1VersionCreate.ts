52705da54ddc7c4c005c73c7cf9c8f4077fd6041
Willie Ruemmele <willieruemmele@gmail.com>
2022-07-29 08:40:59 -0600

    test: refactor tests back into multiple files, fix competing stubs

--- src/package1/package1VersionCreate.ts
+++ src/package1/package1VersionCreate.ts
@@ -5,12 +5,40 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import * as os from 'os';
-import { Connection, Lifecycle, Messages, PollingClient } from '@salesforce/core';
+import { Connection, Lifecycle, Messages, PollingClient, StatusResult } from '@salesforce/core';
 import { Duration } from '@salesforce/kit';
 import { Package1VersionCreateRequest, PackagingSObjects } from '../interfaces';
 
 Messages.importMessagesDirectory(__dirname);
 const messages = Messages.loadMessages('@salesforce/packaging', 'messages');
+
+const packageUploadRequestStatus = async (
+  id: string,
+  connection: Connection,
+  timeout: number,
+  frequency: number
+): Promise<StatusResult> => {
+  const pollingResult = await connection.tooling.sobject('PackageUploadRequest').retrieve(id);
+  switch (pollingResult.Status) {
+    case 'SUCCESS':
+      return { completed: true, payload: pollingResult };
+    case 'IN_PROGRESS':
+    case 'QUEUED':
+      timeout -= frequency;
+      await Lifecycle.getInstance().emit('package1VersionCreate:progress', { timeout, pollingResult });
+
+      return { completed: false, payload: pollingResult };
+    default: {
+      if (pollingResult?.Errors?.errors?.length > 0) {
+        throw messages.createError('package1VersionCreateCommandUploadFailure', [
+          (pollingResult.Errors.errors.map((e: Error) => e.message) as string[]).join(os.EOL),
+        ]);
+      } else {
+        throw messages.createError('package1VersionCreateCommandUploadFailureDefault');
+      }
+    }
+  }
+};
 
 export async function package1VersionCreate(
   connection: Connection,
@@ -19,35 +47,14 @@ export async function package1VersionCreate(
 ): Promise<PackagingSObjects.PackageUploadRequest> {
   const createRequest = await connection.tooling.sobject('PackageUploadRequest').create(options);
   if (pollingOptions.timeout.seconds) {
-    let timeout = pollingOptions.timeout.seconds;
+    const timeout = pollingOptions.timeout.seconds;
     const pollingClient = await PollingClient.create({
-      poll: async () => {
-        const pollingResult = await connection.tooling.sobject('PackageUploadRequest').retrieve(createRequest.id);
-        switch (pollingResult.Status) {
-          case 'SUCCESS':
-            return { completed: true, payload: pollingResult };
-          case 'IN_PROGRESS':
-          case 'QUEUED':
-            timeout -= pollingOptions.frequency.seconds;
-            await Lifecycle.getInstance().emit('package1VersionCreate:progress', { timeout, pollingResult });
-
-            return { completed: false, payload: pollingResult };
-          default: {
-            if (pollingResult?.Errors?.errors?.length > 0) {
-              throw messages.createError('package1VersionCreateCommandUploadFailure', [
-                (pollingResult.Errors.errors.map((e: Error) => e.message) as string[]).join(os.EOL),
-              ]);
-            } else {
-              throw messages.createError('package1VersionCreateCommandUploadFailureDefault');
-            }
-          }
-        }
-      },
+      poll: () => packageUploadRequestStatus(createRequest.id, connection, timeout, pollingOptions.frequency.seconds),
       ...pollingOptions,
     });
     return pollingClient.subscribe<PackagingSObjects.PackageUploadRequest>();
   } else {
-    // jsforce templates weren't working when setting the type to PackageUploadRequest, so we have to cast `as unkown as PackagingSObjects.PackageUploadRequest`
+    // jsforce templates weren't working when setting the type to PackageUploadRequest, so we have to cast `as unknown as PackagingSObjects.PackageUploadRequest`
     return (await connection.tooling
       .sobject('PackageUploadRequest')
       .retrieve(createRequest.id)) as unknown as PackagingSObjects.PackageUploadRequest;
--- test/package1/package1VersionCreate.test.ts
+++ test/package1/package1VersionCreate.test.ts
@@ -4,12 +4,12 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { MockTestOrgData, restoreContext, testSetup } from '@salesforce/core/lib/testSetup';
+import { instantiateContext, MockTestOrgData, restoreContext, stubContext } from '@salesforce/core/lib/testSetup';
 import { Duration } from '@salesforce/kit';
 import { expect } from 'chai';
 import { Connection, Lifecycle } from '@salesforce/core';
 import { assert } from 'sinon';
-import { package1Display, package1VersionCreate } from '../../src/package1';
+import { package1VersionCreate } from '../../src/package1';
 import { PackagingSObjects } from '../../src/interfaces';
 
 const options = {
@@ -38,17 +38,16 @@ const queuedResult = {
   MetadataPackageId: '03346000000MrC0AAK',
 };
 
-describe('Package1 Version Create and Display', () => {
-  const $$ = testSetup();
+describe('Package1 Version Create', () => {
+  const $$ = instantiateContext();
   const testOrg = new MockTestOrgData();
   let conn: Connection;
   let sobjectStub: sinon.SinonStub;
-  let queryStub: sinon.SinonStub;
 
   beforeEach(async () => {
+    stubContext($$);
     await $$.stubAuths(testOrg);
     conn = await testOrg.getConnection();
-    queryStub = $$.SANDBOX.stub(conn.tooling, 'query');
     sobjectStub = $$.SANDBOX.stub(conn.tooling, 'sobject')
       .onFirstCall()
       .returns({
@@ -57,49 +56,10 @@ describe('Package1 Version Create and Display', () => {
       });
   });
 
-  after(() => {
+  afterEach(() => {
     restoreContext($$);
   });
 
-  it('should query and collate data correctly', async () => {
-    queryStub.resolves({
-      done: true,
-      totalSize: 1,
-      records: [
-        {
-          Id: '04t46000001ZfaXXXX',
-          Name: 'Summer 22',
-          MetadataPackageId: '03346000000dmo4XXX',
-          MajorVersion: 1,
-          MinorVersion: 0,
-          PatchVersion: 3,
-          ReleaseState: 'Beta',
-          BuildNumber: 1,
-        },
-      ],
-    });
-    const result = await package1Display(conn, '04t46000001ZfaXXXX');
-    expect(result).deep.equal([
-      {
-        BuildNumber: 1,
-        MetadataPackageId: '03346000000dmo4XXX',
-        MetadataPackageVersionId: '04t46000001ZfaXXXX',
-        Name: 'Summer 22',
-        ReleaseState: 'Beta',
-        Version: '1.0.3',
-      },
-    ]);
-  });
-
-  it('should query and collate data correctly - no results', async () => {
-    queryStub.resolves({
-      done: true,
-      totalSize: 0,
-      records: [],
-    });
-    const result = await package1Display(conn, '04t46000001ZfaXXXX');
-    expect(result).deep.equal([]);
-  });
   it('should send the create request, wait for it to finish, and emit events along the way', async () => {
     sobjectStub
       .onSecondCall()
