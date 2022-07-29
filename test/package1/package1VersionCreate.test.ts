12722f608014041622ab1a807ace360b0c86ec27
Willie Ruemmele <willieruemmele@gmail.com>
2022-07-29 08:58:35 -0600

    chore: os agnostic test

--- test/package1/package1VersionCreate.test.ts
+++ test/package1/package1VersionCreate.test.ts
@@ -4,6 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+import * as os from 'os';
 import { instantiateContext, MockTestOrgData, restoreContext, stubContext } from '@salesforce/core/lib/testSetup';
 import { Duration } from '@salesforce/kit';
 import { expect } from 'chai';
@@ -101,7 +102,7 @@ describe('Package1 Version Create', () => {
       await package1VersionCreate(conn, options, { frequency: Duration.seconds(1), timeout: Duration.minutes(3) });
       assert.fail('the above should throw an error from polling');
     } catch (e) {
-      expect((e as Error).message).to.equal('Package upload failed.\nmessage 1\nmessage 2');
+      expect((e as Error).message).to.equal(`Package upload failed.${os.EOL}message 1${os.EOL}message 2`);
     }
   });
 
