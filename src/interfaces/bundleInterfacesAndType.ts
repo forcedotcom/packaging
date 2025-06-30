ea4df9e0ebc44a75a45cf4d07e7e95d74a080d41
Dawson David <dawson.david@salesforce.com>
2025-06-30 09:03:54 -0600

    feat: created delete method on bundles for package bundle delete
    fix: rebase

--- src/interfaces/bundleInterfacesAndType.ts
+++ src/interfaces/bundleInterfacesAndType.ts
@@ -8,6 +8,7 @@
 import { Nullable } from '@salesforce/ts-types';
 import { Connection, SfProject } from '@salesforce/core';
 import { Duration } from '@salesforce/kit';
+import { SaveResult } from '@jsforce/jsforce-node';
 
 export type BundleCreateOptions = {
   BundleName: string;
@@ -39,3 +40,5 @@ export type BundleInstallOptions = {
     frequency: Duration;
   };
 };
+
+export type BundleSaveResult = SaveResult;
